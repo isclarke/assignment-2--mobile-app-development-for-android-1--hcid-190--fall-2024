001299cb8a785c4478730ed0e7f79719025985fc
isclarke <156957402+isclarke@users.noreply.github.com>
2024-11-13 16:57:38 -0500

    add more import devices

--- app/src/main/java/com/example/matrixprogram/MainActivity.kt
+++ app/src/main/java/com/example/matrixprogram/MainActivity.kt
@@ -18,6 +18,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.remember
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.matrixprogram.ui.theme.MatrixProgramTheme
